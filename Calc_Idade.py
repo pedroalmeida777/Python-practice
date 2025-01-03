3485244fcabf50b36c98c67535b0d2b1805d3a0c
Pedro Almeida <112005029+pedroalmeida777@users.noreply.github.com>
2025-01-02 23:06:23 -0300

    Add UserAge Calculator Project
    Implementing logic to calculate a user's age based on their date of birth.

---
+++ Calc_Idade.py
@@ -0,0 +1,5 @@
+print("Bem-vindo(a) ao Contador de Idades!")
+ano_nasc = int(input("Digite o ano de seu nascimento: "))
+ano_atual = int(input("Digite o ano atual: "))
+idade = ano_atual - ano_nasc 
+print(f"Parabéns! Sua idade é {idade}")
