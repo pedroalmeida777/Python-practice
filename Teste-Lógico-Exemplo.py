0613493f3c646be99dbb94b59d15625cee27b432
Pedro Almeida <112005029+pedroalmeida777@users.noreply.github.com>
2025-01-07 21:50:09 -0300

    Add Logic Test Example
    Adding the basic code for the first logic test in Python.

---
+++ Teste-Lógico-Exemplo.py
@@ -0,0 +1,14 @@
+resposta = "sim"
+texto = "Brasil"
+print("s" in resposta)
+print("n" not in resposta)
+print("b" in texto)
+print("B" in texto)
+
+cidade_p1 = "São Paulo"
+cidade_p2 = "São Paulo"
+cidade_p3 = "Rio de Janeiro"
+print(id(cidade_p1))
+print(id(cidade_p2))
+print(id(cidade_p3))
+print(cidade_p1 is cidade_p2)
